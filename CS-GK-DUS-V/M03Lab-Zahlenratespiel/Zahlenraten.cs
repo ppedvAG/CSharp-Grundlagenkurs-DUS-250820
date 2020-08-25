3071ac28b1ee9f2d747493e321cd69d1fc62ff17
Vadzim Naumchyk <vadzimn@ppedv.de>
2020-08-25 12:43:03 +0200

    M03 done

---
+++ CS-GK-DUS-V/M03Lab-Zahlenratespiel/Zahlenraten.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M03Lab_Zahlenratespiel
+{
+    class Zahlenraten
+    {
+        static void Main(string[] args)
+        {
+            Random generator = new Random();
+            int zufallszahl = generator.Next(1, 5);
+            Console.WriteLine("zufallszahl: {0}", zufallszahl);
+
+            int ratemalzahl;
+            int counter = 0;
+            do
+            {
+                Console.WriteLine("Erraten Sie eine int-Zahl zwischen 1 und 4: ");
+                ratemalzahl = int.Parse(Console.ReadLine());
+                if (zufallszahl == ratemalzahl)
+                {
+                    Console.WriteLine("Sie haben die Zahl erraten");
+                }
+                else if (zufallszahl < ratemalzahl)
+                {
+                    Console.WriteLine("Zufallszahl ist kleiner als Sie denken");
+
+                }
+                else
+                {
+                    Console.WriteLine("Zufallszahl ist größer als Sie denken");
+                }
+                counter++;
+            } while (zufallszahl != ratemalzahl);
+            Console.WriteLine("~~~ Sie haben die Zahl beim {0}. Versuch erraten.", counter);
+            Console.ReadKey();
+        }
+    }
+}
